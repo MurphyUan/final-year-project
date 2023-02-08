f15122f42e80ca3c79817f92f58f88077149dee5
Uan Murphy <G00385338@gmit.ie>
2023-02-08 15:03:43 +0000

    File Initialisation

---
+++ Assets/Scripts/KartController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KartController : MonoBehaviour {
+
+    private void OnEnable()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+}
