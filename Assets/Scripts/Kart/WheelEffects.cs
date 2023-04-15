3389f7d5af194c1514b252469ef0ffa13ad140a0
MurphyUan <G00385338@gmit.ie>
2023-04-15 21:48:44 +0100

    Adding Content to WheelEffects, for playing audio and particle systems

--- Assets/Scripts/Kart/WheelEffects.cs
+++ Assets/Scripts/Kart/WheelEffects.cs
@@ -2,17 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class WheelEffects : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static Transform skidTrailsDetachedParent;
+
+    [SerializeField] private Transform skidTrailPrefab;
+    [SerializeField] private ParticleSystem skidParticles;
+    [SerializeField] private float _skidTrailOffset = 0.1f;
+    public bool IsSkidding { get; private set; }
+    public bool IsPlayingAudio { get; private set; }
+
+    private AudioSource _audioSource;
+    private Transform _skidTrail;
+    private WheelCollider _wheelCollider;
+
+    private void Start()
     {
-        
+        if (skidParticles == null) Debug.Log("No smoke particle systetm found on ", gameObject);
+        else skidParticles.Stop();
+
+        _wheelCollider = GetComponent<WheelCollider>();
+        _audioSource = GetComponent<AudioSource>();
+        IsPlayingAudio = false;
+
+        if (skidTrailsDetachedParent == null)
+            skidTrailsDetachedParent = new GameObject("SkidTrails - Detached").transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void EmitTireSmoke()
     {
-        
+        skidParticles.transform.position = transform.position - transform.up * _wheelCollider.radius;
+        skidParticles.Emit(1);
+        if(!IsSkidding) StartCoroutine("StartSkidTrail");
+    }
+
+    public void PlayAudio()
+    {
+        _audioSource.Play();
+        IsPlayingAudio = true;
+    }
+
+    public void StopAudio()
+    {
+        _audioSource.Stop();
+        IsPlayingAudio = false;
     }
 }
